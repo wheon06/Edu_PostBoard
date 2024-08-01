9be3cbbf6f283210ee80176ebf5566f475e9c471
wheon06 <up2kid@gmail.com>
2024-08-01 16:18:56 +0900

    Initial commit

---
+++ src/main/java/com/wheon/edu_postboard/EduPostBoardApplication.java
@@ -0,0 +1,13 @@
+package com.wheon.edu_postboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPostBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduPostBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wheon/edu_postboard/EduPostBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheon.edu_postboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPostBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
